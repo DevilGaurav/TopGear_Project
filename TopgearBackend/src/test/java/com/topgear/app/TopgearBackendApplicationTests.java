4560eacdf8e536dd3dac425fbdcd88c2d6193995
DevilGaurav <gauravawati3@gmail.com>
2024-08-14 11:09:06 +0530

    backend ready

---
+++ TopgearBackend/src/test/java/com/topgear/app/TopgearBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.topgear.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TopgearBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
