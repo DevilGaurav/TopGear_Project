ae9b106111482a66b41701af73183d31d25d927d
DevilGaurav <gauravawati3@gmail.com>
2024-08-14 11:04:03 +0530

    deleted

--- entities/OrderAggregate/OrderStatus.java
+++
@@ -1,5 +0,0 @@
-package com.app.entities.OrderAggregate;
-
-public enum OrderStatus {
-	Pending, PaymentReceived, PaymentFailed
-}
--- exception/ProductNotFoundException.java
+++
@@ -1,5 +0,0 @@
-package com.app.exception;
-
-public class ProductNotFoundException extends RuntimeException{
-    public ProductNotFoundException(String message){super(message);}
-}
